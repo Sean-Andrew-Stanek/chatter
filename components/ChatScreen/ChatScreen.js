46c99a2ab7ae31fa08cf7ad4a22239f67d8ee279
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-12-01 21:01:32 +0900

    ChatScreen.js => add AsyncStorage

--- components/ChatScreen/ChatScreen.js
+++ components/ChatScreen/ChatScreen.js
@@ -2,42 +2,71 @@ import React from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView} from 'react-native';
 import { PropTypes } from 'prop-types';
 import { useEffect } from 'react';
-import { Bubble, GiftedChat } from 'react-native-gifted-chat';
-import { collection, orderBy, addDoc, onSnapshot, query, /* where */ } from 'firebase/firestore';
-
+import { Bubble, GiftedChat, InputToolbar } from 'react-native-gifted-chat';
+import { collection, orderBy, addDoc, onSnapshot, query, } from 'firebase/firestore';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 import { contrastText, changeAlpha } from '../../color-library';
 import { useState } from 'react';
 
 
-//Chat Bubbles
-const renderBubble = (props, themeColor) => {
-    return (
-        <Bubble
-            {...props}
-            wrapperStyle={{
-                right: {
-                    backgroundColor: changeAlpha(contrastText(themeColor), .2),
-                },
-                left: {
-                    backgroundColor: changeAlpha(contrastText(themeColor), .8),
-                }
-            }}
-        />
-    );
-};
 
 
 //Main Component
-const ChatScreen = ({database, route, navigation}) => {
+const ChatScreen = ({isConnected, database, route, navigation}) => {
+
+    //Chat Bubbles
+    const renderBubble = (props, themeColor) => {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: changeAlpha(contrastText(themeColor), .2),
+                    },
+                    left: {
+                        backgroundColor: changeAlpha(contrastText(themeColor), .8),
+                    }
+                }}
+            />
+        );
+    };
+
+    //Disables the input bar when offline
+    const renderInputToolbar = (props) =>
+    {
+        if(isConnected)
+            return <InputToolbar {...props} />;
+        else
+            return null;
+    };
+
+    //AsyncStorage Database Key
+    let asyncDBKey = 'Messages';
+
+    //Firebase Database Name
+    let firebaseDBName = 'Messages';
 
     const { userID, name, themeColor } = route.params;
     const [messages, setMessages] = useState([]);
 
     const onSend = (newMessages) => {
         /* setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages)); */
-        addDoc(collection(database, 'Messages'), newMessages[0]);
+        addDoc(collection(database, firebaseDBName), newMessages[0]);
     };
 
+    //Store for when the user is offline
+    const cacheDatabase = async(dataToBase) => {
+        try { 
+            await AsyncStorage.setItem('Messages', JSON.stringify(dataToBase));
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    
+    const loadCachedDatabase = async () => {
+        const cacheDatabase = await AsyncStorage.getItem(asyncDBKey) || [];
+        setMessages(JSON.parse(cacheDatabase));
+    };
 
     //LOAD DATABASE
     useEffect(() => {
@@ -45,23 +74,36 @@ const ChatScreen = ({database, route, navigation}) => {
             //Set the Title to the users' name
             navigation.setOptions({ title: name });
 
-            //Desired query
-            const targetQuery = query(collection(database, 'Messages'), /* where('uid', '==', userID),  */orderBy('createdAt', 'desc'));
-            
-            //Callback to unsub onSnapshot
-            const unsubMessages = 
-                onSnapshot(targetQuery, (messagesSnapshot) => {
-                    let newMessages = [];
-                    //PULLS THIS DATA FROM THE DATABASE
-                    messagesSnapshot.forEach(docObject => {
-                        newMessages.push({
-                            id:docObject.id, 
-                            ...docObject.data(),
-                            createdAt: new Date(docObject.data().createdAt.toMillis())});
-                    });
+            //Our callback to unsub to avoid MemLeaks
+            let unsubMessages;
 
-                    setMessages(newMessages);
-                });
+            if(isConnected === true){
+
+                //If there is already a listener, remove it
+                if(unsubMessages)
+                    unsubMessages();
+                unsubMessages = null;
+
+                //Desired query
+                //collection(localDatabase, firebaseDB)
+                const targetQuery = query(collection(database, firebaseDBName), orderBy('createdAt', 'desc'));
+                
+                //Callback to unsub onSnapshot
+                unsubMessages = 
+                    onSnapshot(targetQuery, (messagesSnapshot) => {
+                        let newMessages = [];
+                        //PULLS THIS DATA FROM THE DATABASE
+                        messagesSnapshot.forEach(docObject => {
+                            newMessages.push({
+                                id:docObject.id, 
+                                ...docObject.data(),
+                                createdAt: new Date(docObject.data().createdAt.toMillis())});
+                        });
+                        //Store for offline use
+                        cacheDatabase(newMessages);
+                        setMessages(newMessages);
+                    });
+            } else loadCachedDatabase();
 
             return () => {
                 if(unsubMessages)
@@ -70,13 +112,14 @@ const ChatScreen = ({database, route, navigation}) => {
         } catch(error) {
             console.error('Error in useEffect:', error);
         }
-    }, []);
+    }, [isConnected]);
 
     return (
         <View style={[styles.rootContainer, {backgroundColor: themeColor}]}>
             <GiftedChat
                 messages={messages}
                 renderBubble={(props) => renderBubble(props, themeColor)}
+                renderInputToolbar={(props) => renderInputToolbar(props)}
                 onSend={messages => onSend(messages)}
                 user={{
                     _id: {userID},
@@ -103,7 +146,8 @@ ChatScreen.propTypes = {
     }).isRequired,
     navigation: PropTypes.shape({
         setOptions: PropTypes.func.isRequired,
-    }).isRequired
+    }).isRequired,
+    isConnected: PropTypes.bool.isRequired,
 };
 
 export default ChatScreen;
