07ddac73fb9ee5c2348bf54e61070516b01fdd84
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-12-01 20:11:08 +0900

    App.js => useNetInfo added

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore'
+import { disableNetwork, enableNetwork, getFirestore } from 'firebase/firestore';
+import { useNetInfo } from '@react-native-community/netinfo';
+import { Alert } from 'react-native';
+
 //Recommended by Firebase
-//import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from 'firebase/analytics';
 
 import StartScreen from './components/StartScreen/StartScreen';
 import ChatScreen from './components/ChatScreen/ChatScreen';
@@ -13,20 +16,33 @@ import ChatScreen from './components/ChatScreen/ChatScreen';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+
+    // Monitors the online status of the program
+    const connectionStatus = useNetInfo();
+
+    useEffect(() => {
+        if(connectionStatus.isConnected === false){
+            Alert.alert('Connection lost!');
+            disableNetwork(database);
+        } else if (connectionStatus.isConnected === true) {
+            enableNetwork(database);
+        }
+    }, [connectionStatus.isConnected]);
+
     const firebaseConfig = {
-        apiKey: "AIzaSyAPVSLWVrAURc3W4cVkSvcOyh6iRCjQDok",
-        authDomain: "chatter-17de2.firebaseapp.com",
-        projectId: "chatter-17de2",
-        storageBucket: "chatter-17de2.appspot.com",
-        messagingSenderId: "1014994951167",
-        appId: "1:1014994951167:web:d88461dc704b944ef4e5ca",
-        measurementId: "G-7YQZ179XDG"
-    }
+        apiKey: 'AIzaSyAPVSLWVrAURc3W4cVkSvcOyh6iRCjQDok',
+        authDomain: 'chatter-17de2.firebaseapp.com',
+        projectId: 'chatter-17de2',
+        storageBucket: 'chatter-17de2.appspot.com',
+        messagingSenderId: '1014994951167',
+        appId: '1:1014994951167:web:d88461dc704b944ef4e5ca',
+        measurementId: 'G-7YQZ179XDG'
+    };
 
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
 
-    const database = getFirestore(app)
+    const database = getFirestore(app);
 
     return (
         <NavigationContainer>
@@ -43,6 +59,7 @@ const App = () => {
                 >
                     {props => 
                         <ChatScreen
+                            isConnected = {connectionStatus.isConnected}
                             database = {database}
                             {...props}
                         />
